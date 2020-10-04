89b3810efa82ad43fd03c76dd05bd1b1aa277fe5
Le Haiping <lehaiping@126.com>
2020-10-04 19:07:58 +0800

    %REM% Empty the build floder

--- build/common/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hubert/work/MyCode/HappyBus/common
-# Build directory: /home/hubert/work/MyCode/HappyBus/build/common
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("src")
--- build/common/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hubert/work/MyCode/HappyBus/common/test
-# Build directory: /home/hubert/work/MyCode/HappyBus/build/common/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("types")
-subdirs("props")
